d505449397fb30af176b87795d2e87a9991b49ed
Komodon <matteo.zampiccoli@gmail.com>
2020-11-10 18:25:57 +0100

    Added game and menu (Python files), for drawing tests

---
+++ game.py
@@ -0,0 +1,28 @@
+
+import pygame as pg
+import math, time, random, sys
+
+pygame.init()
+pygame.display.set_caption("A Game of Deception")
+
+FPS = 30
+RES = [800, 600] # [get_monitors()[0].width, get_monitors()[0].height]
+PLAY = True
+
+ 
+def game_loop():
+    while PLAY == True:
+        # Do Player Input here
+        
+        # Do Entity Processing here
+        
+        # Do draw() here
+        
+        draw()
+
+
+def draw():
+    pass
+
+
+game_loop()
